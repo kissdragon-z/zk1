6b7a462cc477744b55874ca8fd29410c60fed6b5
minglongzhang <kissdragon_z@126.com>
2017-06-18 16:43:04 +0800

    test

---
+++ src/com/zl/TestMain.java
@@ -0,0 +1,14 @@
+package com.zl;
+
+/**
+ * Created by zml on 2017/6/15.
+ */
+public class TestMain {
+
+
+    public static void main(String[] args) {
+
+        System.out.println("this is my first mac java class");
+
+    }
+}
