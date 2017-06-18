194c4a023d64530e927278861158bd5526dab2e4
minglongzhang <kissdragon_z@126.com>
2017-06-18 16:46:04 +0800

    test

---
+++ src/com/zl/abs/TestMain.java
@@ -0,0 +1,14 @@
+package com.zl.abs;
+
+/**
+ * Created by zml on 2017/6/18.
+ */
+public class TestMain extends TestAbstractClass {
+
+
+    public void testmethod() {
+        System.out.print("testing ");
+    }
+
+
+}
